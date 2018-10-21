83aefe5330b26e8e0324574b31747b9f85e62a38
jmgerow <justingerow1@gmail.com>
2018-10-21 14:16:27 -0500

    created basic UI

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header = props => (
+ 
+    <div className="row navbar sticky-top header">
+      <div className="col-12 col-md-6 col-lg-8 title">CalSnap</div>  
+      {/* <div className="col-12 col-md-3 col-lg-2 current-score">Current Score:</div>
+      <div className="col-12 col-md-3 col-lg-2 top-score">Top Score: </div> */}
+    </div>
+  
+);
+
+export default Header;
--- client/src/pages/CalorieCount/CalorieCount.js
+++ client/src/pages/CalorieCount/CalorieCount.js
@@ -6,28 +6,42 @@ import {
     Jumbotron,
     Form
 } from "reactstrap";
+import Header from "../../components/Header";
+import Caldisplay from "../../components/Caldisplay";
+import Wrapper from "../../components/Wrapper";
+import Container from "../../components/Container";
+import FoodDisplay from "../../components/FoodDisplay";
+import SnapFoodBtn from "../../components/SnapFoodButton";
 
-const jumbotronStyle = {
-    // background: '#00c2ff',
-    backgroundImage: `url("/images/newspaper.jpg")`,
-    color: 'black',
-    marginLeft: "20%", marginRight: "20%"
-};
+// const jumbotronStyle = {
+//     // background: '#00c2ff',
+//     backgroundImage: `url("/images/newspaper.jpg")`,
+//     color: 'black',
+//     marginLeft: "20%", marginRight: "20%"
+// };
 
 
 class CalorieCount extends Component {
     state = {
+        dailyGoal: 2200,
+        actual: 500,
+        remaining: 0
     };
 
     componentDidMount() {
-        // this.loadsomething();
+        // calculates remaining calories for day
+        this.setState({ remaining: this.state.dailyGoal - this.state.actual })
     }
 
-
+    snapFood = event => {
+        // event.preventDefault();
+        console.log("click is working")
+    }
 
     // handle the form search button to kick off the search to the NYT
     handleSearchSubmit = event => {
         event.preventDefault();
+        
         // API.hitapi({
         // })
         //     .then(res => {
@@ -47,33 +61,54 @@ class CalorieCount extends Component {
 
     render() {
         return (
-            <div>
-                <div className="mainDiv">
-                    Hello World
-    
-                    <div className="modal fade" id="startSurveyModal" tabindex="-1" role="dialog" aria-labelledby="startSurveyModalLabel" aria-hidden="true">
-                        <div className="modal-dialog modal-dialog-centered" role="document">
-                            <div className="modal-content">
-                                <div className="modal-header">
-                                    <h5 className="modal-title modal-text-color" id="startSurveyModalLabel">Tap the picture of your food item!</h5>
-                                </div>
-                                <div className="modal-body">
-                                    <form>
-                                        <br>
-                                            <h6 className="modal-text-color">Tap the video stream to snap your picture!</h6>
-                                            <div>
-                                                <video id="screenshot-video" className="videostream img-fluid" autoPlay="true" preload="auto" muted="true" playsInline="true"></video>
-                                                <img id="screenshot-img" source="">
-                                            </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Wrapper>
+
+                <Header></Header>
+                <Container>
+                    <Caldisplay
+                        dailyGoal={this.state.dailyGoal}
+                        actual={this.state.actual}
+                        remaining={this.state.remaining}
+                    />
+                    <SnapFoodBtn onClick={() => this.snapFood()}/>
+                    <FoodDisplay> 
+                    {/* will map through DB results when built       */}
+                    {/* {this.state.foodItems.map(food-item => (
+                                
+                                ))} */}
+                    </FoodDisplay>
+                </Container>
+
+            </Wrapper>
+            // <div>
+            //     <div className="mainDiv">
+            //         Hello World
+            //         <div className="modal fade" id="startSurveyModal" tabIndex="-1" role="dialog" aria-labelledby="startSurveyModalLabel" aria-hidden="true">
+            //             <div className="modal-dialog modal-dialog-centered" role="document">
+            //                 <div className="modal-content">
+            //                     <div className="modal-header">
+            //                         <h5 className="modal-title modal-text-color" id="startSurveyModalLabel">Tap the picture of your food item!</h5>
+            //                     </div>
+            //                     <div className="modal-body">
+            //                         <form>
+            //                             <br></br>
+            //                             <h6 className="modal-text-color">Tap the video stream to snap your picture!</h6>
+            //                             <div>
+            //                                 {/* <video id="screenshot-video" className="videostream img-fluid" autoPlay="true" preload="auto" muted="true" playsInline="true"></video> */}
+            //                                 <img id="screenshot-img" source=""></img>
+            //                             </div>
+            //                         </form>
+            //                     </div>
+            //                 </div>
+            //             </div>
+            //         </div>
+            //     </div>
+            // </div>
         );
+
     }
-}
-                
+
+};
+
+
 export default CalorieCount;
