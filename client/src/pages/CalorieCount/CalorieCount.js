6f96a75e9c016068a4bf517841b980ebc86e811b
jmgerow <justingerow1@gmail.com>
2018-10-28 23:05:27 -0500

    setup Nutritionix but removed keys for now

--- client/src/pages/CalorieCount/CalorieCount.js
+++ client/src/pages/CalorieCount/CalorieCount.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 // import SomeComponent from "../../components/SomeComponent";
 // import API from "../../utils/API";
+import axios from "axios";
 import './CalorieCount.css';
 import {
     Jumbotron,
@@ -17,17 +18,26 @@ import VideoModal from "../../components/VideoModal";
 import BarcodeModal from "../../components/BarcodeModal";
 import TextInputModal from "../../components/TextInputModal";
 
+
+
 class CalorieCount extends Component {
     state = {
         dailyGoal: 2200,
         actual: 500,
         remaining: 0,
-        isVideoModalOpen: false
+        isVideoModalOpen: false,
+        searchItem: "orange"
     };
 
     componentDidMount() {
         // calculates remaining calories for day
         this.setState({ remaining: this.state.dailyGoal - this.state.actual })
+
+        // temporary location to call nutritionix API
+        // this.nutritionixNutritionSearch()
+        // this.nutritionixInstantSearch()
+        // this.nutritionixBarcode()
+    
     }
 
     toggleModal = () => {
@@ -65,9 +75,69 @@ class CalorieCount extends Component {
         });
     };
 
-    handleIRresponse = response  => {
+    handleIRresponse = response => {
         alert(`Item identified as: ${JSON.stringify(response)}`)
     }
+
+
+    nutritionixInstantSearch = response => {
+        const appKey = process.env.REACT_APP_NUTRITION_KEY
+        const appID = process.env.REACT_APP_NUTRITION_APP_ID
+        
+        axios.get("https://trackapi.nutritionix.com/v2/search/instant?query=" + this.state.searchItem, {
+            headers: {
+                "x-app-id": appID,
+                "x-app-key": appKey,
+            }
+
+
+        }).then(response => { console.log(`got this from nutrionix: ${JSON.stringify(response.data.common[0])}`) })
+
+            .catch(err => { console.log(`got this error from nutrionix: ${err}`) })
+           
+    }
+
+
+    // Nutritionix call for item search
+    nutritionixNutritionSearch = response => {
+        const appKey = ""
+        const appID = ""
+
+        axios.get("https://trackapi.nutritionix.com/v2/natural/nutrients", {
+            headers: {
+                "x-app-id": appID,
+                "x-app-key": appKey,
+                "x-remote-user-id": "0"
+            },
+            body: {
+                "query": "1 cup chicken noodle soup",
+            }
+
+
+        }).then(response => { console.log(`got this from nutrionix: ${JSON.stringify(response.data.common[0])}`) })
+
+            .catch(err => { console.log(`got this error from nutrionix: ${err}`) })
+
+    }
+
+    // Nutritionix call for barcode scanning
+    nutritionixBarcode = response => {
+        const appKey = ""
+        const appID = ""
+
+        axios.get("https://trackapi.nutritionix.com/v2/search/item?nix_item_id=513fc9e73fe3ffd40300109f", {
+            headers: {
+                "x-app-id": appID,
+                "x-app-key": appKey
+            }
+
+
+        }).then(response => { console.log(`got this from nutrionix: ${JSON.stringify(response)}`) })
+
+            .catch(err => { console.log(`got this error from nutrionix: ${err}`) })
+
+    }
+
 
     render(props) {
         return (
@@ -81,16 +151,16 @@ class CalorieCount extends Component {
                     />
                     {/* <SnapFoodBtn onClick={() => this.snapFood()} /> */}
                     <div className="row">
-                    <VideoModal isOpen={this.state.isVideoModalOpen}
-                        onResponseFromIR={this.handleIRresponse}
-                        onClose={this.toggleModal} buttonLabel="Snap Food!">
-                        Here's some content for the modal
+                        <VideoModal isOpen={this.state.isVideoModalOpen}
+                            onResponseFromIR={this.handleIRresponse}
+                            onClose={this.toggleModal} buttonLabel="Snap Food!">
+                            Here's some content for the modal
                     </VideoModal>
-                    <BarcodeModal>
-                        Here's some content for the modal
+                        <BarcodeModal>
+                            Here's some content for the modal
                     </BarcodeModal>
-                    <TextInputModal>
-                        Here's some content for the modal
+                        <TextInputModal>
+                            Here's some content for the modal
                     </TextInputModal>
                     </div>
                     <FoodDisplay>
