df89bc5f6074632cebb4ed32f5203ae6aae49a21
Craig Bomba <talk2cbo@gmail.com>
2018-10-31 18:06:49 -0500

    one more change when calling nutrionix after calling watson

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -82,8 +82,7 @@ module.exports = {
           res.send( { data: "ERR-100: Could not identify item!" } )
         } else {
           console.log(`now going to call nutrionix with the data.....`);
-          nutritionixController.nutritionixInstantSearchDirect(labelsvr)
-          res.send({ data: labelsvr });
+          res.send(nutritionixController.nutritionixInstantSearchDirect(labelsvr))
         }
         // no longer need the image file so remove it!
         fs.unlink(temp, (err) => {
