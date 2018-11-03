5a324683ae03a46f5d1f2733c7e89574bf0ffcea
jgallaway2011 <jgallaway2011@gmail.com>
2018-11-03 15:28:15 -0500

    Updated user to have first name and last name

--- models/User.js
+++ models/User.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  username: { type: String, required: true },
-    food: [{
-      type: Schema.Types.ObjectId,
-      ref: "Food"
-    }]
-
+  user_id: { type: String, required: true },
+  email: { type: String, required: true },
+  food: [{
+    type: Schema.Types.ObjectId,
+    ref: "Food"
+  }]
 });
 
 const User = mongoose.model("User", UserSchema);
