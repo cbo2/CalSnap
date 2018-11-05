6172290d0d4037bd8a54e7f31978226634dd0aaf
Craig Bomba <talk2cbo@gmail.com>
2018-11-05 09:33:49 -0600

    revert BarcodModal back to master and make one small change to css to put up a target for barcode scanning

--- client/src/components/BarcodeModal/BarcodeModal.js
+++ client/src/components/BarcodeModal/BarcodeModal.js
@@ -2,15 +2,11 @@ import React from 'react';
 import { Button, Modal, Row, Col, ModalHeader, ModalBody, Form, FormGroup, Input } from 'reactstrap';
 import API from "../../utils/API";
 import "./BarcodeModal.css";
-import Quagga from 'quagga';
-
 
 class BarcodeModal extends React.Component {
     constructor(props) {
         super(props);
         this.onResponseFromBarcode = this.onResponseFromBarcode.bind(this);
-        this.checkCapabilities = this.checkCapabilities.bind(this);
-        this.searchBarcode = this.searchBarcode.bind(this);
         this.state = {
             modal: false,
             constraints: {
@@ -49,7 +45,7 @@ class BarcodeModal extends React.Component {
         this.setState({ firstDisplay: "reveal" });
         this.setState({ secondDisplay: "d-none" });
         this.toggle();
-    }
+      }
 
     initMedia = () => {
         console.log(`********* initMedia *******`)
@@ -112,116 +108,7 @@ class BarcodeModal extends React.Component {
             console.log(`the preferred Device id is: ${this.state.preferredDevice.deviceId}`)
         }
         console.log(`the constraints is: ${JSON.stringify(this.state.constraints)}`)
-        console.log(`====================== before fakemethod  ======== ${this.state.preferredDevice.deviceId} =================`)
-        navigator.mediaDevices.getUserMedia(this.state.constraints).then(this.gotStream).then(this.gotDevices).then(this.searchBarcode).catch(this.handleError)
-    }
-
-    searchBarcode = (target) => {
-
-        console.log(`====================== setting callback for quagga onDetected =========================`)
-        Quagga.onDetected(detected => {
-            console.log(`*** we got this barcode detected:  [${detected.codeResult.code}]`)
-        })
-
-        Quagga.onProcessed(result => {
-            // console.log(`========> inside onProcessed with result: ${result} <============`)
-            let drawingCtx = Quagga.canvas.ctx.overlay,
-                drawingCanvas = Quagga.canvas.dom.overlay;
-
-            if (result) {
-                // console.log(`========> inside onProcessed with result.boxes: ${result.boxes} <============`)
-                if (result.boxes) {
-                    drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-                    result.boxes.filter(function (box) {
-                        // console.log(`========> inside onProcessed return box != result.box <============`)
-                        return box !== result.box;
-                    }).forEach(function (box) {
-                        console.log(`========> inside onProcessed should be drawing green box now!!!!! <============`)
-                        Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "green", lineWidth: 2 });
-                    });
-                }
-
-                if (result.box) {
-                    Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "#00F", lineWidth: 2 });
-                }
-
-                if (result.codeResult && result.codeResult.code) {
-                    Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
-                }
-            }
-        })
-
-        new Promise(resolve => setTimeout(resolve, 3000)).then(respons => {
-            console.log(`====================== init'ng quagga  ========${this.state.preferredDevice.deviceId}=================`)
-            Quagga.init({
-                inputStream: {
-                    type : "LiveStream",
-                    target: this.video,
-                    constraints: {
-                        width: {min: 640},
-                        height: {min: 480},
-                        facingMode: "environment",
-                        aspectRatio: {min: 1, max: 2}
-                    }
-                },
-                locator: {
-                    patchSize: "medium",
-                    halfSample: true
-                },
-                numOfWorkers: 2,
-                frequency: 10,
-                decoder: {
-                    readers : [{
-                        format: "ean_reader",
-                        config: {}
-                    }],
-                    multiple: false
-                },
-                locate: true
-            }, function (err) {
-                if (err) {
-                    console.log(`**** Quagga Error: ${err}`);
-                    return
-                }
-                console.log("Initialization finished. Ready to start");
-                // let track = Quagga.CameraAccess.getActiveTrack();
-                // let capabilities = {};
-                // if (typeof track.getCapabilities === 'function') {
-                //     capabilities = track.getCapabilities();
-                // }
-                Quagga.start();
-            })
-        })
-    }
-
-    checkCapabilities = () => {
-        let track = Quagga.CameraAccess.getActiveTrack();
-        let capabilities = {};
-        if (typeof track.getCapabilities === 'function') {
-            capabilities = track.getCapabilities();
-        }
-        this.applySettingsVisibility('zoom', capabilities.zoom);
-        this.applySettingsVisibility('torch', capabilities.torch);
-    }
-
-
-    applySettingsVisibility = (setting, capability) => {
-        // depending on type of capability
-        if (typeof capability === 'boolean') {
-            let node = document.querySelector('input[name="settings_' + setting + '"]');
-            if (node) {
-                node.parentNode.style.display = capability ? 'block' : 'none';
-            }
-            return;
-        }
-        if (window.MediaSettingsRange && capability instanceof window.MediaSettingsRange) {
-            let node = document.querySelector('select[name="settings_' + setting + '"]');
-            if (node) {
-                this.updateOptionsForMediaRange(node, capability);
-                node.parentNode.style.display = 'block';
-            }
-            return;
-        }
+        navigator.mediaDevices.getUserMedia(this.state.constraints).then(this.gotStream).then(this.gotDevices).catch(this.handleError)
     }
 
     handleError = (error) => {
@@ -284,7 +171,7 @@ class BarcodeModal extends React.Component {
         this.setState({ secondDisplay: "d-none" })
         this.selectQuantity();
     }
-
+   
     handleQuantity = (event) => {
         event.preventDefault();
         console.log("quantity: " + this.state.quantity)
@@ -319,8 +206,8 @@ class BarcodeModal extends React.Component {
                     <ModalHeader className={this.state.firstDisplay} toggle={this.toggle}>Touch image to snap barcode!</ModalHeader>
                     <ModalHeader className={this.state.secondDisplay} toggle={this.toggle}>Enter number of servings to eat:</ModalHeader>
                     <ModalBody>
-                        <div id="videoimage"  className={this.state.firstDisplay} >
-                            <video ref={video => { this.video = video }}  onClick={this.videoOnClick} className="videoInsert img-fluid" playsInline autoPlay />
+                        <div className={this.state.firstDisplay}>
+                            <video ref={video => { this.video = video }} onClick={this.videoOnClick} className="videoInsert img-fluid" playsInline autoPlay />
                             <img ref={image => { this.image = image }} alt="food pic" className="d-none" />
                             <canvas ref={canvas => { this.canvas = canvas }} className="d-none" />
                         </div>
