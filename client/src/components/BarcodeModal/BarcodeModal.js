dc8f92d805c18b1a6363a6decefdebf48e1cd4a3
Craig Bomba <talk2cbo@gmail.com>
2018-11-05 00:09:46 -0600

    more changes for barcode front end

--- client/src/components/BarcodeModal/BarcodeModal.js
+++ client/src/components/BarcodeModal/BarcodeModal.js
@@ -157,17 +157,28 @@ class BarcodeModal extends React.Component {
                 inputStream: {
                     name: "Live",
                     type: "LiveStream",
-                    target: this.video,    // Or '#yourElement' (optional)
+                    target: this.video,
                     constraints: {
                         width: 640,
                         height: 480,
                         facingMode: "environment",
                         deviceId: this.state.preferredDevice.deviceId
-                    }
+                    },
+                    area: { // defines rectangle of the detection/localization area
+                        top: "0%",    // top offset
+                        right: "0%",  // right offset
+                        left: "0%",   // left offset
+                        bottom: "0%"  // bottom offset
+                    },
+                    singleChannel: false // true: only the red color-channel is read
                 },
                 decoder: {
                     // readers: ["ean_reader", "code_128_reader"]
-                    readers: ["ean_reader"]
+                    readers: ["ean_reader"],
+                    debug: {
+                        drawBoundingBox: true
+                    },
+                    multiple: false
                 }
             }, function (err) {
                 if (err) {
@@ -317,7 +328,7 @@ class BarcodeModal extends React.Component {
                     <ModalHeader className={this.state.secondDisplay} toggle={this.toggle}>Enter number of servings to eat:</ModalHeader>
                     <ModalBody>
                         <div className={this.state.firstDisplay}>
-                            <div ref={viewport => { this.viewport = viewport}}></div>
+                            <div ref={viewport => { this.viewport = viewport }}></div>
                             <video ref={video => { this.video = video }} onClick={this.videoOnClick} className="videoInsert img-fluid" playsInline autoPlay />
                             <img ref={image => { this.image = image }} alt="food pic" className="d-none" />
                             <canvas ref={canvas => { this.canvas = canvas }} className="d-none" />
