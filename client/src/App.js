2da58c3ea16a885250954d1cff62e69f07827e49
jgallaway2011 <jgallaway2011@gmail.com>
2018-11-06 19:31:55 -0600

    Added userProfile page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
 import CalorieCount from "./pages/CalorieCount";
 import Callback from "./pages/Callback";
+import UserProfile from "./pages/UserProfile";
 import NoMatch from "./pages/NoMatch.js";
 import './App.css';
 
@@ -13,6 +14,7 @@ const App = (props) => (
       <Switch>
         <Route exact path="/" render={() => <CalorieCount {...props} />} />
         <Route path="/callback" render={() => <Callback {...props} />} />
+        <Route path="/userprofile" render={() => <UserProfile {...props} />} />
         <Route component={NoMatch} />
       </Switch>
     </div>
