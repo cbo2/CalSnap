2eb665de57f2b03a60c9b954e17a1face40c19d2
jgallaway2011 <jgallaway2011@gmail.com>
2018-11-08 20:49:15 -0600

    Fixed serving_qty and added user input form fields

--- client/src/components/BarcodeModal/BarcodeModal.js
+++ client/src/components/BarcodeModal/BarcodeModal.js
@@ -257,7 +257,7 @@ class BarcodeModal extends React.Component {
                                         <Input
                                             type="select"
                                             name="mealSelect"
-                                            id="mealSelect"
+                                            id="meal-select"
                                             className="form-control form-control-sm"
                                             value={this.state.selectedMeal}
                                             onChange={e => this.setState({ selectedMeal: e.target.value })}
