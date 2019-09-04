689e9f0a1edd5943ac9b5f11684b75e4f928ef14
Craig Bomba <talk2cbo@gmail.com>
2019-09-03 20:24:12 -0500

    try to fix the camera for ios12

--- client/src/components/VideoModal/VideoModal.js
+++ client/src/components/VideoModal/VideoModal.js
@@ -98,8 +98,9 @@ class VideoModal extends React.Component {
           preferred_device = deviceInfo    // take a camera of some kind
         } else {
           // if (deviceInfo.label === "Back Camera") {
-          if (deviceInfo.label.match('[Bb]ack|rear|environment')) {     // regex to match for back/Back camera
-            console.log(`now setting the preffered device to back camera: ${JSON.stringify(deviceInfo)}`)
+          if (deviceInfo.label.match('rear|environment')) {     // regex to match for back/Back camera
+              // if (deviceInfo.label.match('[Bb]ack|rear|environment')) {     // regex to match for back/Back camera
+                console.log(`now setting the preffered device to back camera: ${JSON.stringify(deviceInfo)}`)
             preferred_device = deviceInfo   // prefer the back camera!
           }
         }
