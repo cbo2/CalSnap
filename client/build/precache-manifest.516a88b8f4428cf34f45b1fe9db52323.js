9c210b75959ad8e37d5817f9a7fc9e61e1088bc3
jgallaway2011 <jgallaway2011@gmail.com>
2021-03-11 00:45:57 -0500

    Updating Node version

---
+++ client/build/precache-manifest.516a88b8f4428cf34f45b1fe9db52323.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "4551f3c1298208832aab",
+    "url": "/static/css/main.219e2656.chunk.css"
+  },
+  {
+    "revision": "4551f3c1298208832aab",
+    "url": "/static/js/main.4551f3c1.chunk.js"
+  },
+  {
+    "revision": "7881445c77c44053ffcc",
+    "url": "/static/css/1.e69588aa.chunk.css"
+  },
+  {
+    "revision": "7881445c77c44053ffcc",
+    "url": "/static/js/1.7881445c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fe2cc579bebe91e365ca98844a33c7cc",
+    "url": "/static/media/logo-01.fe2cc579.png"
+  },
+  {
+    "revision": "9d67c02e3fca3f9dbb2cc1d16b3f2ee1",
+    "url": "/static/media/tech.9d67c02e.png"
+  },
+  {
+    "revision": "0be07412a1c196e7cf302e54d02fea48",
+    "url": "/static/media/NutritionixAPI.0be07412.png"
+  },
+  {
+    "revision": "ef5d8f1a4fff2b1a6f9bb9f659b80412",
+    "url": "/static/media/launch-back.ef5d8f1a.jpg"
+  },
+  {
+    "revision": "30573394fe28c9fbd5a1812572670a05",
+    "url": "/index.html"
+  }
+];
